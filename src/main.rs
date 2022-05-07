54c4e24d650d7600c26d21b3f55362c8f06e6c80
Orlando Vargas <nojipiz@protonmail.com>
2022-05-07 10:40:53 -0500

    Added module to deserialize plain json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+//use debug_tree::*;
+mod read_elements;
+use read_elements::LanguageElements;
+
 fn main() {
-    println!("Hi mom!");
+    let language_element: LanguageElements = read_elements::get_language_elements();
+    print!("{:?}", language_element);
 }
+
+// fn print_tree() {
+//     defer_write!("output.txt");
+//     add_branch!("A Fibonacci Tree");
+//     add_leaf!("{}", 3); // <~ THE MAGIC LINE
+// }
---
+++ src/read_elements.rs
@@ -0,0 +1,64 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+pub fn get_language_elements() -> LanguageElements {
+    let input_content = read_input();
+    let plain_elements: PlainElements = serde_json::from_str(&input_content).unwrap();
+    let language_elements = create_language_elements_from(plain_elements);
+    language_elements
+}
+
+fn read_input() -> String {
+    let input_content: String = fs::read_to_string("input.json").unwrap_or(String::from("Error"));
+    if input_content == "Error" {
+        panic!("input.json file not found, please create one or change the read permissions");
+    }
+    input_content
+}
+
+fn create_language_elements_from(plain_elements: PlainElements) -> LanguageElements {
+    let plain_productions: Vec<String> = plain_elements.productions;
+    let productions: Vec<Production> = plain_productions
+        .into_iter()
+        .map(|production| generate_production_from_string(production))
+        .collect();
+    LanguageElements {
+        non_terminals: plain_elements.non_terminals,
+        terminals: plain_elements.terminals,
+        productions,
+        start_symbol: plain_elements.start_symbol,
+    }
+}
+
+fn generate_production_from_string(plain_production: String) -> Production {
+    let elements: Vec<&str> = plain_production.split("->").collect();
+    if elements.len() != 2 {
+        panic!("Bad production syntax, check the productions at input.json file")
+    }
+    Production {
+        init: elements[0].to_owned(),
+        result: elements[1].to_owned(),
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct LanguageElements {
+    pub non_terminals: Vec<String>,
+    pub terminals: Vec<String>,
+    pub productions: Vec<Production>,
+    pub start_symbol: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Production {
+    pub init: String,
+    pub result: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct PlainElements {
+    non_terminals: Vec<String>,
+    terminals: Vec<String>,
+    productions: Vec<String>,
+    start_symbol: String,
+}
