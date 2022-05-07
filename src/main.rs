d482e90e381bae3275f562e9f7c538145f121197
Orlando Vargas <nojipiz@protonmail.com>
2022-05-07 11:56:38 -0500

    Added simple symbols generation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
-//use debug_tree::*;
+use debug_tree::*;
 mod read_elements;
-use read_elements::LanguageElements;
+
+use read_elements::{LanguageElements, Production};
 
 fn main() {
     let language_element: LanguageElements = read_elements::get_language_elements();
-    print!("{:?}", language_element);
+    let tree = TreeBuilder::new();
+    print_tree(&language_element, &tree, &language_element.start_symbol);
+    tree.print();
+    //tree.peek_write("output.txt").unwrap()
 }
 
-// fn print_tree() {
-//     defer_write!("output.txt");
-//     add_branch!("A Fibonacci Tree");
-//     add_leaf!("{}", 3); // <~ THE MAGIC LINE
-// }
+fn print_tree(language: &LanguageElements, tree: &TreeBuilder, current_symbol: &String) {
+    tree.add_branch(current_symbol);
+    let productions = get_avaliable_productions(&language.productions, current_symbol.to_owned());
+    productions.into_iter().for_each(|production| {
+        tree.enter();
+        print_tree(language, tree, production);
+        tree.exit();
+    });
+}
+
+fn get_avaliable_productions(
+    productions: &Vec<Production>,
+    current_symbol: String,
+) -> Vec<&String> {
+    productions
+        .into_iter()
+        .filter(|production| production.init == current_symbol)
+        .map(|production| &production.result)
+        .collect()
+}
