c75c6cb24b43435169c6b5729a2f792d9ee435e9
Orlando Vargas <nojipiz@protonmail.com>
2022-05-07 12:11:33 -0500

    Added limit generation of levels

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,30 @@ use read_elements::{LanguageElements, Production};
 fn main() {
     let language_element: LanguageElements = read_elements::get_language_elements();
     let tree = TreeBuilder::new();
-    print_tree(&language_element, &tree, &language_element.start_symbol);
+    print_tree(&language_element, &tree, &language_element.start_symbol, 0);
     tree.print();
     //tree.peek_write("output.txt").unwrap()
 }
 
-fn print_tree(language: &LanguageElements, tree: &TreeBuilder, current_symbol: &String) {
+fn print_tree(
+    language: &LanguageElements,
+    tree: &TreeBuilder,
+    current_symbol: &String,
+    mut level: u32,
+) {
+    if level > 5 {
+        return;
+    }
     tree.add_branch(current_symbol);
     let productions = get_avaliable_productions(&language.productions, current_symbol.to_owned());
     productions.into_iter().for_each(|production| {
-        tree.enter();
-        print_tree(language, tree, production);
-        tree.exit();
+        production.chars().into_iter().for_each(|symbol| {
+            tree.enter();
+            level += 1;
+            print_tree(language, tree, &symbol.to_string(), level);
+            level -= 1;
+            tree.exit();
+        });
     });
 }
 
