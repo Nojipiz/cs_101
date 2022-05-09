8eb5c9d803d6173fd924a6e515cbd3ba21e517ac
Orlando Vargas <nojipiz@protonmail.com>
2022-05-08 19:50:57 -0500

    Refactor code to other modules

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,19 @@ use debug_tree::*;
 mod full_parse_tree;
 mod particular_tree;
 mod read_elements;
+mod write_elements;
 
 use full_parse_tree::generate_full_parse_tree;
 use particular_tree::generate_particular_tree;
 use read_elements::LanguageElements;
+use write_elements::write_results;
 
 fn main() {
     let language_element: LanguageElements = read_elements::get_language_elements();
+
     let tree = TreeBuilder::new();
     generate_full_parse_tree(&language_element, &tree, &language_element.start_symbol, 0);
-    tree.peek_write("output.txt").unwrap();
-    generate_particular_tree(&language_element);
+
+    let path: Vec<String> = generate_particular_tree(&language_element);
+    write_results(&tree, path, language_element)
 }
---
+++ src/write_elements.rs
@@ -0,0 +1,48 @@
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
+use debug_tree::TreeBuilder;
+
+use crate::read_elements::LanguageElements;
+
+pub fn write_results(
+    full_tree: &TreeBuilder,
+    path_to_word: Vec<String>,
+    language: LanguageElements,
+) {
+    full_tree.peek_write("output.txt").unwrap();
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("output.txt")
+        .unwrap();
+
+    if let Err(e) = {
+        writeln!(
+            file,
+            "
+
+        --------------PARTICULAR TREE RESULTS---------------
+            "
+        )
+    } {
+        eprintln!("Couldn't write to file: {}", e);
+    }
+    if path_to_word.is_empty() {
+        let _sucessfull = writeln!(
+            file,
+            "The word {:?} can't be generated from the current language
+            {:?} ∉ L(G)",
+            &language.word_to_verify, language.word_to_verify
+        );
+    } else {
+        let _sucessfull = write!(
+            file,
+            "{}->{}
+            {:?} ∈ L(G)",
+            language.start_symbol,
+            path_to_word.join("->"),
+            language.word_to_verify
+        );
+    }
+}
