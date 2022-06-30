09a1602960e01f3f23835fb52c267df10934fce0
Orlando Vargas <nojipiz@protonmail.com>
2022-06-30 12:37:48 -0500

    Added reader and file path in arguments of main

--- pseudocode_to_cpp/src/main.rs
+++ pseudocode_to_cpp/src/main.rs
@@ -1,3 +1,11 @@
+use std::env::{self};
+
+use reader::reader::get_plain_pseudocode;
+
+mod reader;
+
 fn main() {
-    println!("Hi mom!");
+    let args: Vec<String> = env::args().collect();
+    let pseudocode: Vec<String> = get_plain_pseudocode(args[1].clone());
+    println!("{:?}", pseudocode);
 }
---
+++ pseudocode_to_cpp/src/reader/reader.rs
@@ -0,0 +1,10 @@
+use std::fs;
+
+pub fn get_plain_pseudocode(path: String) -> Vec<String> {
+    let plain_text: String = fs::read_to_string(path).unwrap();
+    plain_text
+        .split("\n")
+        .map(|line| line.to_string())
+        .filter(|line| !line.is_empty())
+        .collect()
+}
