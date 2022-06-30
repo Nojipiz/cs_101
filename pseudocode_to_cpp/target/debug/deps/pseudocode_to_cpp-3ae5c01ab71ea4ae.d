14379698170fc8ce15b95ae764e8952d505f6492
Orlando Vargas <nojipiz@protonmail.com>
2022-06-30 11:59:33 -0500

    Added boilerplate code

---
+++ pseudocode_to_cpp/target/debug/deps/pseudocode_to_cpp-3ae5c01ab71ea4ae.d
@@ -0,0 +1,5 @@
+/home/nojipiz/Documents/7 Semestre/formales/formal_languages_101/pseudocode_to_cpp/target/debug/deps/pseudocode_to_cpp-3ae5c01ab71ea4ae.rmeta: src/main.rs
+
+/home/nojipiz/Documents/7 Semestre/formales/formal_languages_101/pseudocode_to_cpp/target/debug/deps/pseudocode_to_cpp-3ae5c01ab71ea4ae.d: src/main.rs
+
+src/main.rs:
---
+++ pseudocode_to_cpp/target/debug/deps/pseudocode_to_cpp-bf03fc300c0667b3.d
@@ -0,0 +1,5 @@
+/home/nojipiz/Documents/7 Semestre/formales/formal_languages_101/pseudocode_to_cpp/target/debug/deps/pseudocode_to_cpp-bf03fc300c0667b3.rmeta: src/main.rs
+
+/home/nojipiz/Documents/7 Semestre/formales/formal_languages_101/pseudocode_to_cpp/target/debug/deps/pseudocode_to_cpp-bf03fc300c0667b3.d: src/main.rs
+
+src/main.rs:
