2bd9d247b6fa1110e075dd5a000cb3a70656e4ef
Orlando Vargas <nojipiz@protonmail.com>
2022-07-10 18:16:34 -0500

    Send plain code to lexical

--- pseudocode_to_cpp/src/main.rs
+++ pseudocode_to_cpp/src/main.rs
@@ -1,11 +1,14 @@
-use std::env::{self};
+use std::env;
 
+use lexical::lexical::plain_text_to_tokenized_code;
 use reader::reader::get_plain_pseudocode;
 
 mod reader;
+mod lexical;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let pseudocode: Vec<String> = get_plain_pseudocode(args[1].clone());
-    println!("{:?}", pseudocode);
+    let boxed_pseudocode: Box<Vec<String>> = Box::new(pseudocode);
+    plain_text_to_tokenized_code(boxed_pseudocode);
 }
