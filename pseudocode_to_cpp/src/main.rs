712030d1346ed71ea7da27280de53747207c97a7
Orlando Vargas <nojipiz@protonmail.com>
2022-07-12 14:48:44 -0500

    Formated to pass at syntactic

--- pseudocode_to_cpp/src/main.rs
+++ pseudocode_to_cpp/src/main.rs
@@ -2,13 +2,16 @@ use std::env;
 
 use lexical::lexical::plain_text_to_tokenized_code;
 use reader::reader::get_plain_pseudocode;
+use syntactic::syntactic::check_syntaxis;
 
 mod reader;
 mod lexical;
+mod syntactic;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let pseudocode: Vec<String> = get_plain_pseudocode(args[1].clone());
     let boxed_pseudocode: Box<Vec<String>> = Box::new(pseudocode);
-    plain_text_to_tokenized_code(boxed_pseudocode);
+    let tokenized_code = plain_text_to_tokenized_code(boxed_pseudocode);
+    check_syntaxis(tokenized_code);
 }
--- pseudocode_to_cpp/src/reader/reader.rs
+++ pseudocode_to_cpp/src/reader/reader.rs
@@ -4,7 +4,7 @@ pub fn get_plain_pseudocode(path: String) -> Vec<String> {
     let plain_text: String = fs::read_to_string(path).unwrap();
     plain_text
         .split("\n")
-        .map(|line| line.to_string())
+        .map(|line| line.to_string().trim().to_owned())
         .filter(|line| !line.is_empty())
         .collect()
 }
