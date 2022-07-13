0f976f0db27001f43d8dab9dd4b5904b0f8ef4cb
Nojipiz <nojipiz@protonmail.com>
2022-07-13 11:31:19 -0500

    Add writer module

--- pseudocode_to_cpp/src/main.rs
+++ pseudocode_to_cpp/src/main.rs
@@ -1,17 +1,20 @@
 use std::env;
 
-use lexical::lexical::plain_text_to_tokenized_code;
+use lexical::lexical::{plain_text_to_tokenized_code, Word};
 use reader::reader::get_plain_pseudocode;
 use syntactic::syntactic::check_syntaxis;
+use writer::writer::write_file;
 
 mod reader;
 mod lexical;
 mod syntactic;
+mod writer;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let pseudocode: Vec<String> = get_plain_pseudocode(args[1].clone());
     let boxed_pseudocode: Box<Vec<String>> = Box::new(pseudocode);
     let tokenized_code = plain_text_to_tokenized_code(boxed_pseudocode);
-    check_syntaxis(tokenized_code);
+    check_syntaxis(&tokenized_code);
+    write_file(tokenized_code);
 }
---
+++ pseudocode_to_cpp/src/writer/writer.rs
@@ -0,0 +1,5 @@
+use crate::lexical::lexical::Word;
+
+pub fn write_file(tokenized_code:Vec<Vec<Word>>){
+
+}
